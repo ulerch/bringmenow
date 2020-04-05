bcaa5449643bdcd5686901c20343bc9e10394326
Urs Lerch <mail@ulerch.net>
2020-04-05 15:21:50 +0200

    moving biker with farms to collect food and deliver it to an old woman

--- static/script.js
+++ static/script.js
@@ -1,12 +1,51 @@
 $( document ).ready(function() {
-  var leftPos = 0;
-  var move = function() {
-    $("div.courrier img").css("left", leftPos+'px');
-    leftPos += $("div.courrier img").width() + 1;
-    if( leftPos > $( document).width() - $("div.courrier img").width() ) {
-      leftPos = 0;
+  function move() {
+    $('#courrier').css("left", pos);
+    right = parseInt( $("#courrier").css("left") );
+    console.log( right );
+    if( pos === 0 ) {
+      pos = 10;
+      timeout = 500;
     }
-    setTimeout( move, 1000);
+    else if( pos < endRoute ) {
+      if( right < farm1 ) {
+        pos = parseInt( $('#courrier').css("left") ) + step;
+        timeout = 100;
+      } else if( right < (farm1 + 150) ) {
+        pos = parseInt( $('#courrier').css("left") ) + 180;
+        setTimeout( function() {
+          $('#courrier').attr("src", "static/img/bikecourrier1.gif");
+        }, 250);
+        timeout = 500;
+      } else {
+        if( right < farm2 ) {
+          pos = parseInt( $('#courrier').css("left") ) + step;
+          timeout = 100;
+        } else if( right < (farm2 + 150) ) {
+          pos = parseInt( $('#courrier').css("left") ) + 180;
+          setTimeout( function() {
+            $('#courrier').attr("src", "static/img/bikecourrier2.gif");
+          }, 250);
+          timeout = 500;
+        } else {
+          pos = parseInt( $('#courrier').css("left") ) + step;
+          timeout = 100;
+        }
+      }
+	  } else {
+      pos = 0
+      setTimeout( function() {
+        $('#courrier').attr("src", "static/img/bikecourrier0.gif");
+      }, 500);
+      timeout = 1000;
+    }
+    setTimeout( move, timeout);
   }
+  var step = 10;
+  var pos = 0;
+  var farm1 = parseInt( $("#farm1").css("left") ) - parseInt( $("#courrier").width() );
+  var farm2 = parseInt( $("#farm2").css("left") );
+  var endRoute = parseInt( $("#woman").css("left") ) + parseInt( $("#courrier").width() );
+  var timeout = 100;
   move();
 });
